7a8b74d763627f3168d9a0aca33e5e27083c3afa
brian-h2 <brianheredia200309@gmail.com>
2024-10-10 18:17:15 -0300

    Add mostrar usuario en pantalla

--- client/src/utilities/schems/zod.js
+++ client/src/utilities/schems/zod.js
@@ -3,12 +3,12 @@ import {string, z} from 'zod'
 const zodConfirmation = (formData) => {
     const schema = z.object({
         name: z.string()
-            .min(4, 'Mayor a 4 caracteres el nombre')
-            .max(10, 'No exceda los 10 caracteres en el nombre')
+            .min(3, 'Mayor a 4 caracteres el nombre')
+            .max(15, 'No exceda los 15 caracteres en el nombre')
             .regex(/^[A-Za-z]+$/, 'El nombre solo puede contener letras y sin espacios'),
         username: z.string()
-            .min(6, 'El nombre de usuario debe tener al menos 6 caracteres')
-            .max(10, 'No exceda los 10 caracteres en el usuario'),
+            .min(3, 'El nombre de usuario debe tener al menos 6 caracteres')
+            .max(15, 'No exceda los 15 caracteres en el usuario'),
         email: z.string()
             .email('Por favor, introduce un email válido'),
         password: z.string()
