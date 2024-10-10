d5dcf37bdab5fd905077bd81a58b43eab848b250
brian-h2 <brianheredia200309@gmail.com>
2024-10-10 18:09:51 -0300

    Fix cors

--- server/middlewares/cors.js
+++ server/middlewares/cors.js
@@ -4,14 +4,11 @@ export const corsMiddleware = () => cors({
     origin: (origin, callback) => {
         const acceptedOrigins = [
             "http://localhost:5173", // Cambia este puerto si es necesario
-            "http://127.0.0.1:5173", // También puedes incluir esta variante
-            "http://127.0.0.1:5173/",
-            "http://localhost:5000/", 
-            "http://localhost:5173/",
+            "http://127.0.0.1:5173", 
+            "http://localhost:5000/",
             "https://formulario-venta-de-entradas.vercel.app",
             "https://formulario-venta-de-entradas.vercel.app/",
-            "https://sites.google.com/view/qrentradadigital",
-            "https://1241917164-atari-embeds.googleusercontent.com"
+            "https://sites.google.com" // Permitir acceso desde Google Sites
         ];
         
         // Aceptar solicitudes sin origen (como localhost)
