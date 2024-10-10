8c6e2e8e4b1beb3d17948e14fcd249909380b0c1
brian-h2 <brianheredia200309@gmail.com>
2024-10-10 14:30:14 -0300

    Add user para verificar existencia

--- server/server.js
+++ server/server.js
@@ -33,40 +33,37 @@ app.get('/', (req, res) => {
   res.send('API RESTful - Formulario de Registro');
 })
 
-app.get('/user', (req, res) => {
-  const email = req.query.email; // Obtener el email del usuario de la consulta
+let name;
+
+app.get('/user', async (req, res) => {
+  const email = req.query.email; 
     
   if (!email) {
       return res.status(400).json({ message: "Se requiere un email" });
   }
-  
-  // Consulta a la base de datos
-  connection.query('SELECT username FROM UsuariosRegister WHERE email = ?', [email], (err, results) => {
-      if (err) {
-          console.error('Error en la consulta:', err);
-          return res.status(500).json({ message: "Error en la consulta" });
-      }
-      
-      if (results.length > 0) {
-          return res.json({ username: results[0].username });
-      } else {
-          return res.status(404).json({ message: "Usuario no encontrado" });
-      }
-  });
 
-})
-
-app.get('/usuarios', async (req, res) => {
-  const query = 'SELECT * FROM UsuariosRegister'
+  const query = 'SELECT username FROM UsuariosRegister WHERE email = ?'
+  const values = [email]; 
 
   try {
-    const result = await connection.query(query);
-    res.json(result[0]);
-  } catch (error) {
-    res.status(500).send('Error al obtener los usuarios');
-  }
-})
+    const [result] = await connection.query(query, values); 
+    
+    if (!result[0]) {
+      return res.status(404).json({ message: 'Usuario no encontrado' });
+    } else {
+      res.status(200).json({ username: result[0].username }); 
+      if (name) {
+        res.status(200).json({ message: `Bienvenido ${name}` });
+      } else {
+        res.status(200).json({ message: 'Bienvenido' });
+      }
+    }
 
+    }catch (error) {
+      console.error('Error en la consulta:', error);
+      res.status(500).json({ data: 'Error en el servidor' }); 
+    }
+}) 
 
 app.post('/', async (req, res) => {
   const { email, password } = req.body;
@@ -83,6 +80,7 @@ app.post('/', async (req, res) => {
       // Comparar la contraseña almacenada con la ingresada
       if (user.password == password && user.email == email) { // Aquí puedes usar bcrypt.compare en producción
         res.status(200).send('Logueado correctamente'); // Cambié a 200 para indicar éxito
+        name = user.nombre; // Asignamos el nombre al usuario para el mensaje de bienvenida
       } else {
         res.status(401).send('Email o contraseña incorrectos'); // Respuesta para contraseñas incorrectas
       }
