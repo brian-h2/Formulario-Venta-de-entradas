62baf1683f99d0ae08cddecf652e8586a3466522
brian-h2 <brianheredia200309@gmail.com>
2024-10-11 10:27:40 -0300

    Fix v11

--- server/server.js
+++ server/server.js
@@ -112,7 +112,8 @@ app.post('/register', async (req, res) => {
     if (error.code === 'ER_DUP_ENTRY') {
       return res.status(400).send('El correo electrónico o nombre de usuario ya está registrado');
     }
-    res.status(500).send('Error al crear el usuario',error);
+    console.log(error); //
+    res.status(500).send('Error al crear el usuario');
   }
 });
 
