8685aba9d583ee57c3bd38bd9397273216044f08
brian-h2 <brianheredia200309@gmail.com>
2024-10-14 10:09:53 -0300

    Add datos de usuario en user

--- client/src/components/User.jsx
+++ client/src/components/User.jsx
@@ -15,9 +15,10 @@ const User = () => {
   
         const response = await fetch(emailUrl);
         const data = await response.json();
-        
+
         setEmail(data.email);
-  
+        setTelephone(data.telefono);
+        setUsername(data.nombre);
   
       } catch (error) {
         alert('Error: ' + error.message)
@@ -32,18 +33,18 @@ const User = () => {
   return (
     <div className="flex flex-col w-full mx-auto max-w-lg h-screen justify-center items-center">
       <ul className="space-y-4 text-lg bg-purple-700 text-white p-6 rounded-lg shadow-lg">
-        <h1 className="text-3xl font-bold mb-6">
+        <h1 className="text-3xl font-bold mb-6 text-center	 ">
           Bienvenido {username ? username : 'Invitado'}
         </h1>
         <li className="flex items-center space-x-2">
           <span className="font-medium">Email:</span>
-          <a className="text-purple-200 underline">
+          <a className="text-purple-200">
             {email ? email : 'Usuario'}
           </a>
         </li>
         <li className="flex items-center space-x-2">
           <span className="font-medium">Teléfono:</span>
-          <a className="text-purple-200 underline">
+          <a className="text-purple-200">
             {telephone ? telephone : 'N/A'}
           </a>
         </li>
