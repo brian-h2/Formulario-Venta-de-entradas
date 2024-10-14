c7e0c2e8ad16f400b6935e380aa564e4065ccd0f
brian-h2 <brianheredia200309@gmail.com>
2024-10-14 09:40:21 -0300

    Add estilos al user

--- client/src/components/User.jsx
+++ client/src/components/User.jsx
@@ -2,6 +2,8 @@ import React, { useEffect, useState } from 'react';
 
 const User = () => {
   const [username, setUsername] = useState('');
+  const [email, setEmail] = useState('');
+  const [telephone, setTelephone] = useState('');
 
   useEffect(() => {
 
@@ -14,7 +16,7 @@ const User = () => {
         const response = await fetch(emailUrl);
         const data = await response.json();
         
-        setUsername(data.email);
+        setEmail(data.email);
   
   
       } catch (error) {
@@ -28,9 +30,26 @@ const User = () => {
   }, )
 
   return (
-    <div>
-      <h1>Bienvenido, {username ? username : 'Invitado'}</h1>
-    </div>
+    <div className="flex flex-col w-full mx-auto max-w-lg h-screen justify-center items-center">
+      <ul className="space-y-4 text-lg bg-purple-700 text-white p-6 rounded-lg shadow-lg">
+        <h1 className="text-3xl font-bold mb-6">
+          Bienvenido, {username ? username : 'Invitado'}
+        </h1>
+        <li className="flex items-center space-x-2">
+          <span className="font-medium">Email:</span>
+          <a className="text-purple-200 underline">
+            {email ? email : 'Usuario'}
+          </a>
+        </li>
+        <li className="flex items-center space-x-2">
+          <span className="font-medium">Teléfono:</span>
+          <a className="text-purple-200 underline">
+            {telephone ? telephone : 'N/A'}
+          </a>
+        </li>
+    </ul>
+  </div>
+  
   );
 }
 
