2318845193217e2c1ee2b2f72abe69a4075bd5d6
brian-h2 <brianheredia200309@gmail.com>
2024-10-13 23:00:07 -0300

    Modified user

--- client/src/components/User.jsx
+++ client/src/components/User.jsx
@@ -4,29 +4,28 @@ const User = () => {
   const [username, setUsername] = useState('');
 
   useEffect(() => {
-    const fetchUsername = async () => {
-      // Obtener el email almacenado
-      const email = localStorage.getItem('username'); // O de donde estés almacenando el email
-      
-      if (email) {
-        try {
-          const response = await fetch(`https://formulario-venta-de-entradas-production.up.railway.app/get-email?email=${encodeURIComponent(email)}`);
-          if (response.ok) {
-            const data = await response.json();
-            setUsername(data.username || 'Invitado'); // Asegúrate de que la respuesta contenga el nombre
-          } else {
-            console.error('Error al obtener el nombre de usuario');
-          }
-        } catch (error) {
-          console.error('Error en la solicitud:', error);
-        }
-      } else {
-        setUsername('Invitado'); // Si no hay email almacenado
-      }
-    };
 
-    fetchUsername();
-  }, []);
+    const asyncDates = async () => {
+
+      try {
+      
+        const emailUrl = 'https://formulario-venta-de-entradas-production.up.railway.app/get-email'
+  
+        const response = await fetch(emailUrl);
+        const data = await response.json();
+        
+        setUsername(data.email);
+  
+  
+      } catch (error) {
+        alert('Error: ' + error.message)
+      }
+
+    }
+
+    asyncDates();
+
+  }, )
 
   return (
     <div>
